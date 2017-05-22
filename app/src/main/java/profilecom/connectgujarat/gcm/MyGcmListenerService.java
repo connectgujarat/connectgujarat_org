45bea876a9fe12bb8e5a44a1f6b132e164289fd9
Rohan Kandwal <rohan@nexialabs.com>
2017-05-22 14:36:48 +0530

    MyGcmListenerService - Updated code to show notification related to the news + Added pending intent to load NewsListActivity

--- app/src/main/java/profilecom/connectgujarat/gcm/MyGcmListenerService.java
+++ app/src/main/java/profilecom/connectgujarat/gcm/MyGcmListenerService.java
@@ -6,14 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-
+import android.text.TextUtils;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
 import java.util.Map;
-
-import profilecom.connectgujarat.MainActivity;
+import java.util.Random;
+import profilecom.connectgujarat.NewsListActivity;
 import profilecom.connectgujarat.R;
 
 
@@ -45,8 +45,10 @@ public class MyGcmListenerService extends FirebaseMessagingService {
         String message = remoteMessage.getFrom();
         Map data = remoteMessage.getData();
 
-        sendNotification(message);
-
+        final String title = (String) data.get("title");
+        if (!TextUtils.isEmpty(title)) {
+            sendNotification(title);
+        }
     }
 
     /**
@@ -55,21 +57,21 @@ public class MyGcmListenerService extends FirebaseMessagingService {
      * @param message GCM message received.
      */
     private void sendNotification(String message) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        Intent intent = new Intent(this, NewsListActivity.class);
+        intent.putExtras(new Bundle());
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.sendbird_ic_launcher)
-                .setContentTitle("SendBird")
+                .setContentTitle(getString(R.string.app_name))
                 .setContentText(message)
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setSound(defaultSoundUri);
 
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1000, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationBuilder.setContentIntent(pendingIntent);
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
     }
 }
